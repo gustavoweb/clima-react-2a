2285655f4878d3f06fd20d79f9db1dae8344719e
gustavoweb <contato@gustavoweb.com>
2025-10-01 10:50:19 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="app-container">
+        <div className="content-wrapper">
+          <header>
+            <h1>Consulta de Clima</h1>
+            <p>Exemplo de consumo de API com React</p>
+          </header>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
