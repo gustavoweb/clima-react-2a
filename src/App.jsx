9914b44e973724c8eb6b3ff780b12b6466645e3b
gustavoweb <contato@gustavoweb.com>
2025-10-22 09:21:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { CloudSunRain, MapPinned } from 'lucide-react';
 
 function App() {
 
@@ -8,9 +9,34 @@ function App() {
       <div className="app-container">
         <div className="content-wrapper">
           <header>
-            <h1>Consulta de Clima</h1>
+            <h1>
+              <CloudSunRain color="white" size={48} />
+              Consulta de Clima
+            </h1>
             <p>Exemplo de consumo de API com React</p>
           </header>
+
+          <div className="busca-box">
+            <div className="busca-container">
+              <input type="text" />
+              <button>Buscar</button>
+            </div>
+          </div>
+
+          {/* Resultado do Clima */}
+          <div className="card-resultado">
+            <div className="info-cidade">
+              <div className="nome-cidade">
+                <MapPinned color="red" size={48} />
+                Campinas, BR
+              </div>
+              <p className="desc-cidade">Nublado</p>
+            </div>
+
+            {/* Temperatura Principal */}
+            
+          </div>
+
         </div>
       </div>
     </>
